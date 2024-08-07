025d292112ed1c17019dbd99f6eff2c7a1426f84
YOCELYN12 <yocelynrivera9@gmail.com>
2024-08-07 15:44:46 -0600

    Tablero,adivina color

--- Juego/tablero.py
+++ Juego/tablero.py
@@ -1,4 +1,32 @@
 
+from colored import fg,attr
+
+class Tablero:
+    color = {
+        "r": fg(1), "b": fg(4), "g": fg(2), "y": fg(3)       
+    }
+    
+    def __init__(self):
+        self.color_secreto = []
+        self.turnos = []
+    
+    def definir_color(self,color):
+        self.color_secreto = color
+    
+    def comprobar_color(self,intento):
+        retroalimentacion = []
+        
+        copia_color_adivinar = self.color_secreto.copy()
+        for elemento in range(4):
+            if intento[elemento] == copia_color_adivinar[elemento]:
+                retroalimentacion.append("color_verde")
+            elif intento[elemento] in copia_color_adivinar:
+                retroalimentacion.append("color_amarillo")
+            else: 
+                retroalimentacion.append("color_blanco")
+        return retroalimentacion
+
+
 
             
            
