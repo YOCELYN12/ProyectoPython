9387c40214a1865e8ad554e9fc0532b6deba4654
YOCELYN12 <yocelynrivera9@gmail.com>
2024-08-07 13:45:41 -0600

    Clase para poder jugar

--- Juego/maquina.py
+++ Juego/maquina.py
@@ -6,4 +6,10 @@ class Jugar:
     def __init__(self):
         self.taablero = Tablero()
         self.creador_codigo = None
-        self.adivinador        
+        self.adiviandor_codigo = None
+        
+    def elegir_rol(self):
+        modo_juego = input("Tienes 2 opciones de juego, puedes ser el creador del codigo o el adivinador: ").lower()
+        if modo_juego == "creador del codigo":
+            self.creador_codigo = Creador(True)
+            self.adiviandor_codigo = Adivinador(False)
