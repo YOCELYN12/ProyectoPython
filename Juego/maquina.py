110fd828db26df035b402a49123accabb1dc0d49
YOCELYN12 <yocelynrivera9@gmail.com>
2024-08-07 13:41:41 -0600

    Maquina

--- Juego/maquina.py
+++ Juego/maquina.py
@@ -2,3 +2,8 @@ from jugadores import Adivinador, Creador
 from tablero import Tablero
 
 
+class Jugar:
+    def __init__(self):
+        self.taablero = Tablero()
+        self.creador_codigo = None
+        self.adivinador        
